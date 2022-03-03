c02474193cdb2d8d596ad842de9847bb02e363ba
Mikel Intxaurbe <mikelintxaurbe@mpg.io>
2022-03-03 19:27:18 +0100

    Lecture 3 - prep
    - Added moving sideways with 'A' and 'D' and looking up/down with mouse
    - Use camera's coordinate-axis instead of the pawn's coordinate-axis to determine forward and right vectors for movement
    - Made it so whenever we Play the current level, we capture the mouse input by default
    - Set default level in editor to last level loaded

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -4,7 +4,9 @@
 #include "SCharacter.h"
 
 #include <Camera/CameraComponent.h>
+#include <GameFramework/CharacterMovementComponent.h>
 #include <GameFramework/SpringArmComponent.h>
+#include <Kismet/KismetMathLibrary.h>
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -13,10 +15,15 @@ ASCharacter::ASCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->bUsePawnControlRotation = true;
 	SpringArmComp->SetupAttachment(RootComponent);
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	bUseControllerRotationYaw = false;
 }
 
 // Called when the game starts or when spawned
@@ -28,7 +35,26 @@ void ASCharacter::BeginPlay()
 
 void ASCharacter::MoveForward(float Value)
 {
-	AddMovementInput(GetActorForwardVector(), Value);
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0;
+	ControlRot.Roll = 0;
+
+	AddMovementInput(ControlRot.Vector(), Value);
+}
+
+void ASCharacter::MoveRight(float Value)
+{
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0;
+	ControlRot.Roll = 0;
+
+	// UE4 direction axis
+	//	- x: forward (red)
+	//	- y: right (green)
+	//	- z: up (blue)
+	const FVector RightVector = UKismetMathLibrary::GetRightVector(ControlRot);
+
+	AddMovementInput(RightVector, Value);
 }
 
 // Called every frame
@@ -44,7 +70,9 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
 	
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
